202cb2fc7a82962254214b16f818a1da442577b2
GuillermoITIC <156217545+GuillermoITIC@users.noreply.github.com>
2024-04-23 09:38:51 -0600

    main.js

--- js/main.js
+++ js/main.js
@@ -1,34 +1,17 @@
-// Función para obtener datos de la API Mock
-function obtenerUltimaOrden() {
-    return fetch('https://66176ba4ed6b8fa434829b58.mockapi.io/ingresos', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        // Convertir el JSON a un array
-        const array = Object.values(data);
-        console.log(array);
+$(document).ready(function(){
+    // Función para obtener y mostrar los últimos 3 registros de MockAPI
+    function obtenerUltimosRegistros() {
+        $.getJSON('https://66176ba4ed6b8fa434829b58.mockapi.io/ingresos', function(data){
+            // Limpiar el contenido anterior de la lista desordenada
+            $('#ultimaOrdenLista').empty();
+            // Obtener los últimos 5 registros y agregarlos al HTML
+            for (var i = data.length - 5; i < data.length; i++) {
+                var order = data[i];
+                $('#ultimaOrdenLista').append('<li>' + order.accion + ' - ' + order.fecha + '</li>');
+            }
+        });
+    }
 
-        // Obtener el último elemento de los datos recibidos
-        const ultimaOrden = array[array.length - 1]; 
-        // Retornar el texto de la última orden
-        return ultimaOrden.texto;
-    });
-}
-
-// Función para actualizar el texto de la última orden en la página
-function actualizarUltimaOrden() {
-    obtenerUltimaOrden().then(texto => {
-        // Actualizar el texto de la última orden en el span correspondiente
-        document.getElementById("lastOrderText").innerText = texto;
-    });
-}
-
-// Llamar a la función para actualizar la última orden al cargar la página
-actualizarUltimaOrden();
-
-// Establecer un intervalo para actualizar la última orden cada 2 segundos
-setInterval(actualizarUltimaOrden, 2000);
+    // Llamar a la función para obtener los últimos 3 registros cada 2 segundos
+    setInterval(obtenerUltimosRegistros, 2000);
+});
